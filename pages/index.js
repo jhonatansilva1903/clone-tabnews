929082812f987f8fbac2e42d1373ee569689f057
John <jhonatands1993@gmail.com>
2025-04-19 12:29:11 +0000

    adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function home() {
+return (
+    <div>
+        <h1>Ao amor da minha vida &lt;3</h1>
+        <p>Entre curativos e sorrisos, você brilha,</p>
+        <p>Com mãos que curam e alma que cintila.</p>
+        <p>Nosso lar com dois gatinhos a ronronar,</p>
+        <p>É o lugar onde sempre quero estar.</p>
+    </div>
+);
+}export default home;
